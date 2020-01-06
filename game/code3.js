3eecd80c62fb0354f395a65ac4ee978b780ba78a
Asherl1971 <55368644+Asherl1971@users.noreply.github.com>
2020-01-06 10:34:29 +0800

    v1

--- game/code3.js
+++ game/code3.js
@@ -1,22 +1,24 @@
-gdjs.InfoCode = {};
-gdjs.InfoCode.GDBGObjects1= [];
-gdjs.InfoCode.GDBGObjects2= [];
+gdjs.GameOverCode = {};
+gdjs.GameOverCode.GDBGObjects1= [];
+gdjs.GameOverCode.GDBGObjects2= [];
+gdjs.GameOverCode.GDScoreObjects1= [];
+gdjs.GameOverCode.GDScoreObjects2= [];
 
-gdjs.InfoCode.conditionTrue_0 = {val:false};
-gdjs.InfoCode.condition0IsTrue_0 = {val:false};
-gdjs.InfoCode.condition1IsTrue_0 = {val:false};
+gdjs.GameOverCode.conditionTrue_0 = {val:false};
+gdjs.GameOverCode.condition0IsTrue_0 = {val:false};
+gdjs.GameOverCode.condition1IsTrue_0 = {val:false};
 
 
-gdjs.InfoCode.eventsList0xb5aa0 = function(runtimeScene) {
+gdjs.GameOverCode.eventsList0xb5aa0 = function(runtimeScene) {
 
 {
 
 
-gdjs.InfoCode.condition0IsTrue_0.val = false;
+gdjs.GameOverCode.condition0IsTrue_0.val = false;
 {
-gdjs.InfoCode.condition0IsTrue_0.val = gdjs.evtTools.input.wasKeyReleased(runtimeScene, "Space");
-}if (gdjs.InfoCode.condition0IsTrue_0.val) {
-{gdjs.evtTools.runtimeScene.replaceScene(runtimeScene, "Game", false);
+gdjs.GameOverCode.condition0IsTrue_0.val = gdjs.evtTools.input.wasKeyReleased(runtimeScene, "Space");
+}if (gdjs.GameOverCode.condition0IsTrue_0.val) {
+{gdjs.evtTools.runtimeScene.replaceScene(runtimeScene, "MainMenu", false);
 }}
 
 }
@@ -25,10 +27,26 @@ gdjs.InfoCode.condition0IsTrue_0.val = gdjs.evtTools.input.wasKeyReleased(runtim
 {
 
 
-gdjs.InfoCode.condition0IsTrue_0.val = false;
+gdjs.GameOverCode.condition0IsTrue_0.val = false;
 {
-gdjs.InfoCode.condition0IsTrue_0.val = gdjs.evtTools.runtimeScene.sceneJustBegins(runtimeScene);
-}if (gdjs.InfoCode.condition0IsTrue_0.val) {
+gdjs.GameOverCode.condition0IsTrue_0.val = gdjs.evtTools.runtimeScene.sceneJustBegins(runtimeScene);
+}if (gdjs.GameOverCode.condition0IsTrue_0.val) {
+gdjs.GameOverCode.GDScoreObjects1.createFrom(runtimeScene.getObjects("Score"));
+{for(var i = 0, len = gdjs.GameOverCode.GDScoreObjects1.length ;i < len;++i) {
+    gdjs.GameOverCode.GDScoreObjects1[i].setString(gdjs.evtTools.common.getVariableString(runtimeScene.getGame().getVariables().getFromIndex(0)));
+}
+}}
+
+}
+
+
+{
+
+
+gdjs.GameOverCode.condition0IsTrue_0.val = false;
+{
+gdjs.GameOverCode.condition0IsTrue_0.val = gdjs.evtTools.runtimeScene.sceneJustBegins(runtimeScene);
+}if (gdjs.GameOverCode.condition0IsTrue_0.val) {
 {gdjs.evtTools.sound.playSound(runtimeScene, "Assets\\sound\\Room_For_Two.mp3", true, 100, 1);
 }}
 
@@ -44,17 +62,19 @@ gdjs.InfoCode.condition0IsTrue_0.val = gdjs.evtTools.runtimeScene.sceneJustBegin
 }
 
 
-}; //End of gdjs.InfoCode.eventsList0xb5aa0
+}; //End of gdjs.GameOverCode.eventsList0xb5aa0
 
 
-gdjs.InfoCode.func = function(runtimeScene) {
+gdjs.GameOverCode.func = function(runtimeScene) {
 runtimeScene.getOnceTriggers().startNewFrame();
 
-gdjs.InfoCode.GDBGObjects1.length = 0;
-gdjs.InfoCode.GDBGObjects2.length = 0;
+gdjs.GameOverCode.GDBGObjects1.length = 0;
+gdjs.GameOverCode.GDBGObjects2.length = 0;
+gdjs.GameOverCode.GDScoreObjects1.length = 0;
+gdjs.GameOverCode.GDScoreObjects2.length = 0;
 
-gdjs.InfoCode.eventsList0xb5aa0(runtimeScene);
+gdjs.GameOverCode.eventsList0xb5aa0(runtimeScene);
 return;
 
 }
-gdjs['InfoCode'] = gdjs.InfoCode;
+gdjs['GameOverCode'] = gdjs.GameOverCode;
